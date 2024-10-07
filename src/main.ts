09e5a506ca4767cc1dbc922060536885f1651374
Choconaldo <audioafrfb@gmail.com>
2024-10-07 09:13:56 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+const nomeEl = document.getElementById("nome");
+const idadeEl = document.getElementById("idade");
+const emailEl = document.getElementById("email");
+const resultadoEl = document.getElementById("resultado");
