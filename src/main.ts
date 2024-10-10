eb04f3d2dd4d67546aaa7b6bed2b4dcbc9684ee0
Choconaldo <audioafrfb@gmail.com>
2024-10-10 16:29:18 -0300

    logic done

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
-// DOM elements
+// DOM elements capture
 
 const taskInputEl = document.getElementById("taskInput") as HTMLInputElement;
 const taskListEl = document.getElementById("taskList") as HTMLDivElement;
 const addButtonEl = document.getElementById("addButton") as HTMLButtonElement;
+const removeTaskEl = document.getElementById("removeTask") as HTMLDivElement;
 
 // Task list array
 
-let taskList: string[] = getTasks();
+let taskList: string[];
 
-renderTaskList();
+// Add task function
 
 function addTask() {
 	const newtask = taskInputEl.value;
 	taskList.push(newtask);
 	localStorage.setItem("@TASK", JSON.stringify(taskList));
 	taskInputEl.value = "";
-	renderTaskList();
+	initApp();
 }
 
 // Add task button
@@ -39,10 +40,57 @@ function getTasks() {
 function renderTaskList() {
 	let taskToBeRendered = "";
 	let taskListInMemoryArray = getTasks();
-	if (taskListInMemoryArray) {
-		taskListInMemoryArray.forEach((task: string) => {
-			taskToBeRendered += `<li>${task}</li>`;
+	if (taskListInMemoryArray.length) {
+		taskListInMemoryArray.forEach((task: string, index: number) => {
+			taskToBeRendered += `<li>Tarefa nº ${index + 1}: ${task}</li>`;
 			taskListEl.innerHTML = taskToBeRendered;
 		});
+	} else {
+		taskListEl.innerHTML = "";
 	}
 }
+
+// Render remove task function
+
+function renderRemoveTask() {
+	if (taskList.length) {
+		let taskNumber = "";
+		taskList.forEach((task: string, index: number) => {
+			taskNumber += `<option value="${index}">${index + 1}</option>`;
+		});
+		taskNumber = `<h2>Exclusão de tarefas:</h2><span>Excluir a tarefa nº</span>
+        <select id="select">
+          ${taskNumber}
+        </select>
+        <button id="removeButton">Excluir</button>`;
+
+		removeTaskEl.innerHTML = taskNumber;
+		const selectEl = document.getElementById("select") as HTMLSelectElement;
+		document
+			.getElementById("removeButton")!
+			.addEventListener("click", () => {
+				removeTask(Number(selectEl.value));
+			});
+	} else {
+		removeTaskEl.innerHTML = "";
+	}
+}
+
+// Remove task function
+
+function removeTask(taskIndex: number) {
+	let tasks = getTasks();
+	tasks.splice(taskIndex, 1);
+	localStorage.setItem("@TASK", JSON.stringify(tasks));
+	initApp();
+}
+
+// Init App function
+
+function initApp() {
+	taskList = getTasks();
+	renderTaskList();
+	renderRemoveTask();
+}
+
+initApp();
