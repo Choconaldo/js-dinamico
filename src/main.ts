4d72f2b21403ca176242c845336fdcb4ab9b2c55
Choconaldo <audioafrfb@gmail.com>
2024-10-14 13:53:06 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+// DOM elements capture
+
+const cepEl = document.getElementById("cep") as HTMLInputElement;
+const ruaEl = document.getElementById("rua") as HTMLInputElement;
+const cidadeEl = document.getElementById("cidade") as HTMLInputElement;
+const estadoEl = document.getElementById("estado") as HTMLInputElement;
+
+// CEP validation
+
+if (cepEl) {
+	cepEl.onblur = function cepValidation() {
+		const cepValue = cepEl.value;
+		try {
+			if (cepValue.length === 8 && Number(cepValue)) {
+				const cepReturn = fetch(
+					`https://viacep.com.br/ws/${cepEl.value}/json/`
+				);
+				cepReturn
+					.then((response) => response.json())
+					.then((processedResponse) => {
+						ruaEl.value = processedResponse.logradouro;
+						cidadeEl.value = processedResponse.localidade;
+						estadoEl.value = processedResponse.estado;
+					});
+			} else {
+				alert("Formato do CEP digitado inválido");
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+}
